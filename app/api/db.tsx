e1ebe5148261df3757dfe41411bd5649cb71b43c
AS-Aurora <aayushsharma.lucky97@gmail.com>
2024-12-13 23:52:13 +0530

    Learning to write some backend code using Next JS

---
+++ app/api/db.tsx
@@ -0,0 +1,8 @@
+const books = [
+    {id: 1, name: '1...'}, 
+    {id: 2, name: '2...'}, 
+    {id: 3, name: '3...'}, 
+
+]
+
+export default books
--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,24 @@ import { log } from "console";
 import Image from "next/image";
 import Hello from "./components/Hello";
 
-export default function Home() {
-  console.log("Home page");
-  
+export default async function Home() {
+
+    const response = await fetch('https://dummyjson.com/products')
+    if(!response.ok) throw new Error('failed to fetch data')
+      const data = await response.json()
+      const albums = data.products
+
+    console.log(albums);
+    
   return (
     <>
     <h1>Welcome to Next js</h1>
-    <Hello />
+
+    {albums.map((album: {id: number, title: string}) => (
+      <div key={album.id}>
+        <h3>{album.title}</h3>
+      </div>
+    ))}
     </>
   );
 }
